a54fa1cedb0c941a51f32cc0d9f82485bed2aa5c
jzzyy <passby158@gmail.com>
2023-01-02 17:51:31 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,13 +1,41 @@
 "use strict";
-const hello = (req,res) => {
-    res.render("home/index");
+
+const output = {
+    hello: (req,res) => {
+        res.render("home/index");
+    },
+    
+    login: (req,res) => {
+        res.render("home/login");
+    },
+}
+
+const users = {
+    id: ["이재현","passby","259"],
+    psword: ["8468","1234","0000"],
 };
 
-const login = (req,res) => {
-    res.render("home/login");
-};
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+        
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하였습니다.",
+        });
+    }
+}
 
 module.exports = {
-    hello,
-    login,
+    output,
+    process,
 };
